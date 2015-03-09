48592f751b8c3d8ec44ddd40552fd788ede5835b
David <davidCdong_123@hotmail.com>
2015-03-08 21:41:25 -0400

    better image gallery

--- image-gallery.js
+++ image-gallery.js
@@ -3,12 +3,14 @@ var dcd_IG_images;
 var dcd_IG_SET_WIDTH;
 var dcd_IG_CURRENT_INDEX = 0;
 var dcd_IG_MAX_COUNTER;
-var dcd_IG_IS_ANIMATING = false;
+var dcd_IG_IS_ANIMATING_IMAGES = false;
+var dcd_IG_ControlIsShown = false;
+var dcd_IG_controlAnimation;
 
 //Function to add acceleration to animation
 function dcd_IG_calcSpeed(counter){
 	var s;
-	//if(counter <= MAX_COUNTER/2.0){
+	//if(counte r <= MAX_COUNTER/2.0){
 	//	s = -1 * (counter-MAX_COUNTER/2.0)*(counter+MAX_COUNTER/2.0) * (25.0/Math.pow(MAX_COUNTER/2.0,2));
 	//}else{
 	//	s = -1 * (counter-MAX_COUNTER/2.0*3)*(counter-MAX_COUNTER/2.0) * (25.0/Math.pow(MAX_COUNTER/2.0,2));
@@ -20,14 +22,14 @@ function dcd_IG_calcSpeed(counter){
 }
 
 function dcd_IG_slideLeft(firstTime, counter, currentMargin){
-	if(dcd_IG_IS_ANIMATING && firstTime){
+	if(dcd_IG_IS_ANIMATING_IMAGES && firstTime){
 		return;
 	}
 	if(dcd_IG_CURRENT_INDEX+1 == dcd_IG_images.length){
 		return;
 	}
 	if(firstTime){
-		dcd_IG_IS_ANIMATING = true;
+		dcd_IG_IS_ANIMATING_IMAGES = true;
 		dcd_IG_images[0].style.zIndex = 0;
 		dcd_IG_images[dcd_IG_CURRENT_INDEX+1].style.zIndex = 1;
 		currentMargin = parseInt(window.getComputedStyle(dcd_IG_images[0],null).getPropertyValue("margin-left"));
@@ -36,7 +38,7 @@ function dcd_IG_slideLeft(firstTime, counter, currentMargin){
 		dcd_IG_images[0].style.marginLeft = currentMargin - dcd_IG_SET_WIDTH + "px";
 		dcd_IG_images[dcd_IG_CURRENT_INDEX+1].style.marginLeft = 0 + "px";
 		dcd_IG_CURRENT_INDEX++;
-		dcd_IG_IS_ANIMATING = false;
+		dcd_IG_IS_ANIMATING_IMAGES = false;
 		return;
 	}
 
@@ -50,7 +52,7 @@ function dcd_IG_slideLeft(firstTime, counter, currentMargin){
 }
 
 function dcd_IG_slideRight(firstTime, counter, currentMargin){
-	if(dcd_IG_IS_ANIMATING && firstTime){
+	if(dcd_IG_IS_ANIMATING_IMAGES && firstTime){
 		return;
 	}
 	if(dcd_IG_CURRENT_INDEX == 0){
@@ -58,7 +60,7 @@ function dcd_IG_slideRight(firstTime, counter, currentMargin){
 	}
 
 	if(firstTime){
-		dcd_IG_IS_ANIMATING = true;
+		dcd_IG_IS_ANIMATING_IMAGES = true;
 		dcd_IG_images[0].style.zIndex = 1;
 		dcd_IG_images[dcd_IG_CURRENT_INDEX].style.zIndex = 0;
 		currentMargin = parseInt(window.getComputedStyle(dcd_IG_images[0],null).getPropertyValue("margin-left"));
@@ -67,7 +69,7 @@ function dcd_IG_slideRight(firstTime, counter, currentMargin){
 		dcd_IG_images[0].style.marginLeft = currentMargin + dcd_IG_SET_WIDTH + "px";
 		dcd_IG_images[dcd_IG_CURRENT_INDEX].style.marginLeft = 0 + "px";
 		dcd_IG_CURRENT_INDEX--;
-		dcd_IG_IS_ANIMATING = false;
+		dcd_IG_IS_ANIMATING_IMAGES = false;
 		return;
 	}
 
@@ -78,6 +80,48 @@ function dcd_IG_slideRight(firstTime, counter, currentMargin){
 	setTimeout(function(){
 		dcd_IG_slideRight(false, counter++, currentMargin);
 	},dcd_IG_calcSpeed(counter));
+}
+
+function dcd_IG_showControls(leftBtn, rightBtn){
+	if(dcd_IG_ControlIsShown){
+		return;
+	}
+	dcd_IG_ControlIsShown = true;
+	if(dcd_IG_controlAnimation){
+		clearInterval(dcd_IG_controlAnimation);
+	}
+
+	var adder = 2;
+	var end = 0;
+
+	dcd_IG_controlAnimation = setInterval(function(){
+		leftBtn.style.left = parseInt(window.getComputedStyle(leftBtn,null).getPropertyValue("left")) + adder + "px";
+		rightBtn.style.right = parseInt(window.getComputedStyle(rightBtn,null).getPropertyValue("right")) + adder + "px";
+		if(parseInt(window.getComputedStyle(leftBtn,null).getPropertyValue("left")) == end){
+			clearInterval(dcd_IG_controlAnimation);
+		}
+	},3);
+}
+
+function dcd_IG_hideControls(leftBtn, rightBtn){
+	if(!dcd_IG_ControlIsShown){
+		return;
+	}
+	dcd_IG_ControlIsShown = false;
+	if(dcd_IG_controlAnimation){
+		clearInterval(dcd_IG_controlAnimation);
+	}
+
+	var adder = -2;
+	var end = -parseInt(window.getComputedStyle(leftBtn,null).getPropertyValue("width"));
+
+	dcd_IG_controlAnimation = setInterval(function(){
+		leftBtn.style.left = parseInt(window.getComputedStyle(leftBtn,null).getPropertyValue("left")) + adder + "px";
+		rightBtn.style.right = parseInt(window.getComputedStyle(rightBtn,null).getPropertyValue("right")) + adder + "px";
+		if(parseInt(window.getComputedStyle(leftBtn,null).getPropertyValue("left")) == end){
+			clearInterval(dcd_IG_controlAnimation);
+		}
+	},3);
 }
 
 var DCD_ImageGalleryProto = Object.create(HTMLElement.prototype, {
@@ -100,6 +144,9 @@ var DCD_ImageGalleryProto = Object.create(HTMLElement.prototype, {
 			leftBtn.addEventListener("click", function(){dcd_IG_slideLeft(true, 0, 0);});
 			var rightBtn = this.shadowRoot.getElementById("rightButton");
 			rightBtn.addEventListener("click", function(){dcd_IG_slideRight(true, 0, 0);});
+
+			container.addEventListener("mouseover", function(){dcd_IG_showControls(leftBtn, rightBtn);});
+			container.addEventListener("mouseleave", function(){dcd_IG_hideControls(leftBtn, rightBtn);});
 		}
 	}
 });
